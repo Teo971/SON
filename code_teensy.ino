413455ba2018acaf3fa6e44bbbab01875bfd5731
Tharathathal <129197979+Tharathathal@users.noreply.github.com>
2024-02-15 08:14:17 +0100

    Update code_teensy.ino

--- code_teensy.ino
+++ code_teensy.ino
@@ -11,18 +11,24 @@ AudioConnection patchCord2(tp,1,out,1);
 
 void setup() {
   Serial.begin(9600);
+  pinMode(0, INPUT);
   AudioMemory(6);
   audioShield.enable();
   audioShield.inputSelect(AUDIO_INPUT_MIC);
   audioShield.micGain(10); // in dB
-  audioShield.volume(0.9);
+  audioShield.volume(0.8);
+  tp.setParamValue("fl", 200);
+  tp.setParamValue("fu", 10000);
+  tp.setParamValue("window", 1000);
+  tp.setParamValue("xfade", 10);
   Serial.println("Starting...");
 }
 
 void loop() {
-  float shift = random(-120,120)/10.0;
-  //float shift = 3.0;
+  float shift = analogRead(A0)/42.625 - 12.0;
   Serial.println(shift);
+  //float shift = random(-120,120)/10.0;
+  //float shift = 3.0;
   tp.setParamValue("shift",shift);
   delay(500);
 }
